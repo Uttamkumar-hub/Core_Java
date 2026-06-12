790d918cf68dfe845b88832452cc3f0632a2445c
Uttamkumar-hub <uttamkumarkalva@gmail.com>
2026-06-12 17:16:21 +0530

    Task

---
+++ src/Task_20_02/ATM_Balance.java
@@ -0,0 +1,15 @@
+package Task_20_02;
+
+public class ATM_Balance {
+	public static void Balance(String name, int balance) {
+		System.out.println("Name of the Acccount Holder:"+name);
+		System.out.println("Current Balance:"+balance);
+	}
+
+	public static void main(String[] args) {
+		Balance("Kalva Uttam Kumar", 5000);
+		
+
+	}
+
+}
